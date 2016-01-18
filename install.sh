a0ffa9fed3538c4035527c810ce48f55111b00bf
Tennessee Leeuwenburg <t.leeuwenburg@bom.gov.au>
2016-01-18 13:59:52 +1100

    Adapted install shell script from https://github.com/tleeuwenburg/pyconau15/blob/master/install.sh
    Confirmed currently works with ./install.sh on OSX from commandline with Anaconda installed. Need to push 'y' now and again to confirm the install steps. Could consolidate and quiet this, but it gives some feedback on progress. Should work on Linux also but might need to confirm with some folks.

---
+++ install.sh
@@ -0,0 +1,17 @@
+#Download and install Anaconda: http://docs.continuum.io/anaconda/install.html
+
+conda create -p ./mlenv python=3.4 numpy scipy scikit-learn pandas xray pillow
+source activate ./mlenv
+
+conda install cython
+conda install numpy scipy scikit-learn pandas xray pillow
+conda install ipython ipython-notebook matplotlib seaborn
+pip install theano
+pip install keras
+pip install tweepy
+
+git clone https://github.com/danielfrg/word2vec
+cd word2vec
+python setup.py install
+cd ..
+rm -rf word2vec
