ffa6193c7251c9ec0def59a7203621e40e4cfd96
Tennessee Leeuwenburg <t.leeuwenburg@bom.gov.au>
2016-02-04 16:09:54 +1100

    Updated install

--- install.sh
+++ install.sh
@@ -1,18 +1,26 @@
 #Download and install Anaconda: http://docs.continuum.io/anaconda/install.html
 
-conda create -p ./mlenv python=3.4 numpy scipy scikit-learn pandas xray pillow
+conda create -p ./mlenv python=3.4 python
 source activate ./mlenv
 
 conda install cython
 conda install numpy scipy scikit-learn pandas xray pillow
 conda install ipython ipython-notebook matplotlib seaborn
 #pip install theano  # note -- keras depends on latest devel
+
+cd source_packages
+git clone https://github.com/Theano/Theano.git
 pip install -e ./source_packages/Theano/
+cd ../
+
+
 pip install keras
 pip install tweepy
 
+cd source_packages
 git clone https://github.com/danielfrg/word2vec
 cd word2vec
 python setup.py install
 cd ..
 rm -rf word2vec
+cd ..
